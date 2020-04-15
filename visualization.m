3171c78aa56de6e383132d65b205eba1a38a360f
kaczmarekkacper <kaaczmarekkacper@gmail.com>
2020-04-15 18:26:24 +0200

    dodanie wyników testów

--- visualization.m
+++ visualization.m
@@ -1,4 +1,5 @@
-input = fopen('input.txt', 'r');
+path = "results/6/";
+input = fopen(sprintf("%sinput.txt", path), 'r');
 for i = 1:4
     param = fgets(input);
     eval(param);
@@ -18,7 +19,7 @@ end
 rectangle('Position', [0 0 width height]);
 axis([0-radius, width+radius, 0-radius, height+radius]);
 grid on;
-output = fopen('output.txt', 'r');
+output = fopen(sprintf("%soutput.txt", path), 'r');
 while ~feof(output)
     circles = fgets(output);
     eval(circles);
